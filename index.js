7817d1d97ebff73bb3ff95e3185b365d35248bcf
Manoj_Pant <manojpant097@gmail.com>
2023-06-25 21:30:46 +0530

    add a controller and a router forhome

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const app = express();
 const port = 8000;
 
 
+
+
+// use express router 
+
+app.use('/', require('./routes'));
+
+
+
+
+
 app.listen(port,function(err){
     if(err){
-        console.log(`Error in running the server:${err}`);
+        console.log(`Error in running the server:${err}`); // interpolation
     }
     console.log(`Server is running on port: ${port}`);
 });
