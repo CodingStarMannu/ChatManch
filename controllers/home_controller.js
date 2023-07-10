3bdd6afa57524120367d73a158d33f896bee1901
Manoj_Pant <manojpant097@gmail.com>
2023-07-10 09:29:22 +0530

    setup static file access for layouts and pages

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,6 @@
 module.exports.home = function(req,res){
-    return res.end('<h1>Express is Up for ChatManch!</h1>')
+
+    return res.render('home',{
+        title: "Home"
+    });
 }
--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 
 
+// Set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
-// use express router 
 
+// Use express router
 app.use('/', require('./routes'));
 
-
-//set up the view engine
-app.use('view engine', 'ejs');
-app.set('views','./views');
-
-
-
-
-app.listen(port,function(err){
-    if(err){
-        console.log(`Error in running the server:${err}`); // interpolation
+app.listen(port, function(err) {
+    if (err) {
+        console.log(`Error in running the server: ${err}`);
     }
     console.log(`Server is running on port: ${port}`);
 });
