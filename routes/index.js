68a42c10932ed52c12dc47903d8c0b428c261d20
Manoj_Pant <manojpant097@gmail.com>
2023-08-09 08:52:26 +0530

    actions and routes setup for likes

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,8 @@ router.use('/posts', require('./posts'));
 
 router.use('/comments', require('./comments'));
 
+router.use('/likes', require('./likes'));
+
 
 router.use('/api', require('./api'));
 
