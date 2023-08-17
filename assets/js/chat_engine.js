2a4f5e7247e3280a28571a68533a8118bd325e4f
Manoj_Pant <manojpant097@gmail.com>
2023-08-17 23:50:51 +0530

    socket set up completed

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -1,10 +1,15 @@
+// this is the user side chat engine
 class ChatEngine{
+    //taking user email and chat-box id from frontend
     constructor(chatBoxId, userEmail){
         this.chatBox = $(`#${chatBoxId}`);
         this.userEmail = userEmail;
 
+          //sending the connection request to the observer(server)
+        //'connect' event is fired when new connection is fired
         this.socket = io.connect('http://:5000');
 
+        //if userEmail exists then connectionHandler is called to detect the connection
         if(this.userEmail){
             this.connectionHandler();
         }
@@ -12,8 +17,63 @@ class ChatEngine{
     }
 
     connectionHandler(){
+        let self=this;
+
+        //receives the request if connection is established from observer
         this.socket.on('connect', function(){
             console.log("Connection Established using sockets....");
-        })
+
+        //user sending request to join room named ChatManch and sending some data    
+        self.socket.emit('join_room',{
+            user_email:self.userEmail,
+            chatroom:'Chat-Manch'
+        });
+
+           //receives request when an user joined the chatroom named 'Chat-Manch' by detecting 'user_joined" event
+           self.socket.on('user_joined',function(data){
+            console.log('a user joined',data);
+        });
+    });
+
+        // CHANGE :: send a message on clicking the send message button
+        $('#send-message').click(function(){
+            let msg = $('#chat-message-input').val();
+
+            if (msg != ''){
+                self.socket.emit('send_message', {
+                    message: msg,
+                    user_email: self.userEmail,
+                    chatroom: 'codeial'
+                });
+            }
+        });
+
+         //receiving self message and other message 
+        self.socket.on('receive_message', function(data){
+            console.log('message received', data.message);
+
+           //displaying message in dom
+            let newMessage = $('<li>');
+
+            let messageType = 'other-message';
+
+            if (data.user_email == self.userEmail){
+                messageType = 'self-message';
+            }
+
+            newMessage.append($('<span>', {
+                'html': data.message
+            }));
+
+            newMessage.append($('<sub>', {
+                'html': data.user_email
+            }));
+
+            newMessage.addClass(messageType);
+
+            $('#chat-messages-list').append(newMessage);
+        });
+
+
     }
 }
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,3 +1,5 @@
+
+
 module.exports.chatSockets = function(socketServer){
     let io = require('socket.io')(socketServer,{
         cors:{
@@ -10,6 +12,26 @@ module.exports.chatSockets = function(socketServer){
 
         socket.on('disconnect', () => {
             console.log('Socket disconnected');
-          });
-    })
+        });
+      //server will receive request by subscriber to join chatroom by detection the 'join_room' event 
+    socket.on('join_room',function(data){
+        console.log('joining request received',data);
+
+        //if request received then socket will join that chatroom
+        // ---> this function will add subscriber to chatroom
+        //if chatroom with name 'Chat-Manch' is already exists then user will enter and
+        // if not exist then socket will create new and user will be enter
+        socket.join(data.chatroom);
+
+        //emit to all the other users in the chatroom that 'an user has joined the room by notifying them'
+        io.in(data.chatroom).emit('user_joined',data);
+    });
+
+    // detect send_message and broadcast to everyone in the room
+    socket.on('send_message', function(data){
+        io.in(data.chatroom).emit('receive_message', data);
+    });
+
+});
+
 }
